8426fd99d69e3c397acea79981be768a11be5883
ivanka08 <ivankahudic@gmail.com>
2023-09-16 18:03:02 +0300

    Task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const list = document.querySelector(".gallery");
+
+list.insertAdjacentHTML("beforeend", createMurkup(galleryItems));
+list.addEventListener("click", handleClick)
+
+function createMurkup(arr) {
+    return arr.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+    `).join("")
+}
+
+function handleClick(event) {
+    event.preventDefault();
+    if (event.target === event.currentTarget) {
+        return;
+    }
+
+const clickedItem = event.target.closest(".gallery__item"); 
+  const original = clickedItem.querySelector("img").getAttribute("data-source");
+  const description = clickedItem.querySelector("img").getAttribute("alt");
+
+    const instance = basicLightbox.create(`
+    <div class="modal">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${original}"
+      alt="${description}"
+    />
+  </a>
+    </div>
+    `);
+
+    instance.show();
+}
+
+
 console.log(galleryItems);
